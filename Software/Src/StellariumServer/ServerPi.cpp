940af44452adb704cc234534274f2763aca35e61
Chris <C_P_T_Dick@yahoo.co.uk>
2015-11-15 16:07:57 +0000

    removed goto slew code
    removed the goto slew code from the server.

--- Software/Src/StellariumServer/ServerPi.cpp
+++ Software/Src/StellariumServer/ServerPi.cpp
@@ -36,11 +36,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 ServerPi::ServerPi(int Port)
             :Server(Port)
 {
-#if 0
-    current_pos[0] = desired_pos[0] = 1.0;
-    current_pos[1] = desired_pos[1] = 0.0;
-    current_pos[2] = desired_pos[2] = 0.0;
-#endif
     next_pos_time = -0x8000000000000000LL;
 }
 
@@ -64,43 +59,6 @@ STATUS (4 bytes,signed integer): status of the telescope, currently unused.
 */
 void ServerPi::Step(int64_t TimeoutMicros)
 {
-#if 0
-    int64_t now = GetNow();
-    if (now >= next_pos_time)
-    {
-        next_pos_time = now + 500000;
-        current_pos[0] = 3*current_pos[0] + desired_pos[0];
-        current_pos[1] = 3*current_pos[1] + desired_pos[1];
-        current_pos[2] = 3*current_pos[2] + desired_pos[2];
-        double h = current_pos[0]*current_pos[0]
-                 + current_pos[1]*current_pos[1]
-                + current_pos[2]*current_pos[2];
-        if (h > 0.0)
-        {
-            h = 1.0 / sqrt(h);
-            current_pos[0] *= h;
-            current_pos[1] *= h;
-            current_pos[2] *= h;
-        }
-        else
-        {
-            current_pos[0] = desired_pos[0];
-            current_pos[1] = desired_pos[1];
-            current_pos[2] = desired_pos[2];
-        }
-        
-        const double ra = atan2(current_pos[1],current_pos[0]);
-        const double dec = atan2(current_pos[2],
-                                 sqrt(current_pos[0]*current_pos[0]+current_pos[1]*current_pos[1]));
-        const uint32_t ra_int = (uint32_t)floor(
-                                       0.5 +  ra*(((uint32_t)0x80000000)/M_PI));
-        const int32_t dec_int = (int32_t)floor(0.5 + dec*(((uint32_t)0x80000000)/M_PI));
-        const int32_t status = 0;
-        SendPosition(ra_int,dec_int,status);
-    }
-    
-    Server::Step(TimeoutMicros);
-#endif
     int64_t now = GetNow();
     if (now >= next_pos_time)
     {
@@ -143,7 +101,6 @@ void ServerPi::SetRaDec (double Ra, double Dec )
 void ServerPi::Run( void )
 {
     TelescopeManager::GetRaDec( &RightAscension, &Declination );
-    //SetRaDec( RightAscension, Declination );
     Step( 10000 );
 }
 
@@ -169,14 +126,6 @@ void ServerPi::GotoReceived(uint32_t ra_int, int32_t dec_int)
     ra_rads = ra_int*(M_PI/(unsigned int)0x80000000);
     dec_rads = dec_int*(M_PI/(unsigned int)0x80000000);
     TelescopeManager::SetGotoTarget(ra_rads, dec_rads);
-#if 0
-    const double ra = ra_int*(M_PI/(uint32_t)0x80000000);
-    const double dec = dec_int*(M_PI/(uint32_t)0x80000000);
-    const double cdec = cos(dec);
-    desired_pos[0] = cos(ra)*cdec;
-    desired_pos[1] = sin(ra)*cdec;
-    desired_pos[2] = sin(dec);
-#endif
 }
 
 
