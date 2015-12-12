79deeb07b558b1f73cb06f00d9850e045b03187d
Chris <C_P_T_Dick@yahoo.co.uk>
2015-12-12 00:02:50 +0000

    adding config file for Camera interface install
    preventing the pop ups from appearing during the installation of the
    RPiCamera interface. The only one left is the reboot prompt, by which
    time we are ready for a reboot to finish and run.

--- install.sh
+++ install.sh
@@ -15,15 +15,7 @@
 
 sudo apt-get -y install libncurses5-dev python-dev pps-tools git-core scons
 
-#rpicam interface
-git clone https://github.com/silvanmelchior/RPi_Cam_Web_Interface.git
-cd RPi_Cam_Web_Interface
-chmod u+x *.sh
-sudo source install.sh
-#add default web interface page
-sudo mkdir /var/www/RPiCam
-sudo cp -r ./www/* /var/www/RPiCam
-cd ../Software
+cd ./Software
 
 #Gps deamon
 wget http://git.savannah.gnu.org/cgit/gpsd.git/snapshot/gpsd-master.tar.gz
@@ -44,12 +36,19 @@ make
 #copy executables to somewhere in the PATH
 sudo cp ./Out/StarPi /usr/local/bin/StarPi
 sudo cp  websocketd /usr/local/bin/websocketd
-
+sudo cp WMM.COF /usr/local/bin/WMM.COF
 cd ..
-sudo mkdir /var/www/StarPi
+#ToDo check /var/www exists before RPiCAMInterface is installed.
 sudo cp -r ./Website/* /var/www/StarPi
 
-
-
-
-
+#rpicam interface
+git clone https://github.com/silvanmelchior/RPi_Cam_Web_Interface.git
+cp RPi_Cam_config.txt ./RPi_Cam_Web_Interface/config.txt
+sudo mkdir /var/www/StarPi
+sudo mkdir /var/www/RPiCam/
+cd RPi_Cam_Web_Interface
+chmod u+x install.sh
+sudo ./install.sh q
+#line above will ask for reboot, so this may should never happen.
+#add default web interface page
+#sudo cp -r ./www/* /var/www/RPiCam
