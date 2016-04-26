aacb97009e401a1770e9cf16827af42a0a397e91
Chris <C_P_T_Dick@yahoo.co.uk>
2016-04-26 18:16:32 +0100

    install script fixes
    trying to sort out the install script so others can install it. still
    have issues with the location of website

--- install.sh
+++ install.sh
@@ -1,26 +1,32 @@
 #! /bin/bash
 #Star Pi
 # please do this first:
-#sudo apt-get update
-#sudo apt-get dist-upgrade
-#sudo rpi-update
-#wheezy
-#sudo sed -i -e "\$adeb http://http.debian.net/debian wheezy-backports main" /etc/apt/sources.list
-#sudo apt-get update
-#sudo apt-get install debian-archive-keyring 
-#sudo apt-get update
-#sudo reboot
 #git clone https://github.com/ChrisDick/StarPi
 #chmod u+x install.sh
-#./install
+#./install.sh
+# then go and make a nice cup of tea.
+sudo sed -i 's/raspberrypi/spacecam/g' /etc/hosts
+sudo sed -i 's/raspberrypi/spacecam/g' /etc/hostname
 
+OSCODENAME=grep VERSION= /etc/os-release | cut -d ' ' -f 2-  | tr -d '"' 
+if [ OSCODENAME -eq "(jessie)" ]
+then
+#we have a jessie based install
+sudo apt-get install scons 
+sudo apt-get install libgps-dev  ???
+else
+#assume it's wheezy
+sudo sed -i -e "\$adeb http://http.debian.net/debian wheezy-backports main" /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install debian-archive-keyring 
+sudo apt-get update
+sudo apt-get -y -t wheezy-backports install scons
+fi
 
-#wheezy 
-sudo apt-get -t wheezy-backports install scons
-#jessie
-#sudo apt-get install scons 
-#sudo apt-get install libgps-dev  ???
-sudo apt-get -y install libncurses5-dev python-dev pps-tools git-core 
+sudo apt-get update
+sudo apt-get dist-upgrade
+sudo rpi-update
+sudo apt-get -y install libncurses5-dev python-dev pps-tools git-core python-smbus i2c-tools
 
 cd ./Software
 
@@ -40,7 +46,11 @@ cd ./../..
 mkdir Obj
 mkdir Out
 make
+# add to library path
+sudo sed -i -e "/usr/local/bin" /etc/ld.so.conf
+sudo ldconfig
 #copy executables to somewhere in the PATH
+
 sudo cp ./Out/StarPi /usr/local/bin/StarPi
 sudo cp  websocketd /usr/local/bin/websocketd
 sudo chmod u+x /usr/local/bin/websocketd
@@ -53,12 +63,10 @@ sudo mkdir /var/www
 sudo mkdir /var/www/html
 sudo mkdir /var/www/html/StarPi
 sudo mkdir /var/www/html/RPiCam
-sudo mkdir /var/www/html/StarPi
 sudo cp -r ./Website/* /var/www/html/StarPi
 cd RPi_Cam_Web_Interface
 sudo cp -r ./www/* /var/www/html/RPiCam
 
 chmod u+x install.sh
 sudo ./install.sh q
-#line above will ask for reboot, so this should never happen.
-#add default web interface page
+#line above will ask for reboot, this is the last thing we need to do.
